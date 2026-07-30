5d163efe970e984926ea031c19904f393c045284
PranjalPrasad27 <79925210+PranjalPrasad@users.noreply.github.com>
2026-07-30 18:21:35 +0530

    first commit

---
+++ src/main/java/com/quo/quotation2/Quotation2Application.java
@@ -0,0 +1,13 @@
+package com.quo.quotation2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Quotation2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Quotation2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/quo/quotation2/Quotation2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quo.quotation2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Quotation2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
